9fcb294d5b135586b1edf2133c6c553bc72c359b
rayyseoffline-rgb <rayyseoffline@gmail.com>
2026-08-23 14:55:19 +0430

    Add initial app.js with system check functionality

---
+++ static/app.js
@@ -0,0 +1,18 @@
+console.log("RAYYSE OFFLINE System Loaded");
+
+function systemCheck(){
+
+    let time = new Date();
+
+    console.log(
+        "System Online:",
+        time
+    );
+
+}
+
+window.onload = function(){
+
+    systemCheck();
+
+};
